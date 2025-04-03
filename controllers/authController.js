15abbfd27a673dd26f92db99757e2a5215b4f3e4
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-03 16:40:13 +0000

    Add sign up functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,14 @@
 const prisma = require("../config/prisma");
 
+async function createUser(req, res) {
+  const { username, password } = req.body;
+  await prisma.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+  res.json(req.body)
+}
 
-
-module.exports = {  };
+module.exports = { createUser };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,5 +2,7 @@ const { Router } = require("express")
 const authRouter = Router()
 const authController = require("../controllers/authController")
 
+authRouter.route("/sign-up", authController.createUser)
+
 
 module.exports = authRouter
