55bce6f3545388a980495b89ed78e37b22b9f7f9
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-03 18:26:47 +0000

    Enable password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,13 @@
 const prisma = require("../config/prisma");
+const bcrypt = require("bcryptjs");
 
 async function createUser(req, res) {
   const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10)
   await prisma.user.create({
     data: {
       username,
-      password,
+      password: hashedPassword,
     },
   });
   res.json(req.body)
