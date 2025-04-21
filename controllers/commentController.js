e79035880b6564e19c0eee7107cd4e238a9aac6a
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-21 09:49:20 +0000

    Include author data when fetching comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,7 +1,11 @@
 const prisma = require("../config/prisma");
 
 async function getAllComments(req, res) {
-  const comments = await prisma.comment.findMany();
+  const comments = await prisma.comment.findMany({
+    include : {
+      author: true
+    }
+  });
   res.json(comments)
 }
 
