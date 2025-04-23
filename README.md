8f4d173672ef9c7795e2f65df4be08deac3bfb41
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-23 12:49:56 +0000

    Add dedicated route for getting all post

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # blog-api-backend
+
+if database cors error happen
+check logs on render
+reset database 
+using npx prisma migrate reset
+npx prisma generate
+npx prisma db push
--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const express = require("express");
 const app = express();
 const postRouter = require("./routes/postRouter");
 const authRouter = require("./routes/authRouter");
-const cors = require("cors")
+const cors = require("cors");
+
+// const corsOptions = {
+//   origin: [
+//     "https://blog-api-users-ires0gf1l-jyrexronaldos-projects.vercel.app/",
+//     "https://blog-api-admin-93axs6972-jyrexronaldos-projects.vercel.app/",
+//     "https://blog-api-admin.vercel.app/",
+//     "https://blog-api-users.vercel.app/"
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
+// origin: <FROTNEND-URL>,
+// credentials: true,
+// methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
+
+app.use(cors());
 
 app.use(express.json());
-
-app.use(cors())
 
 app.use("/auth", authRouter);
 app.use("/", postRouter);
